99906655e52100bd5451b7c41ea316b46fb1935f
limuy <limuyang202011@163.com>
2024-05-29 07:42:00 +0800

    add resources

--- scripts/weapons/star_wrath.gd
+++ scripts/weapons/star_wrath.gd
@@ -1,11 +1,19 @@
 extends Sprite2D
 
+var start_flag = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func wave():
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if !start_flag:
+		return
+	self.transform = self.transform.rotated_local(0.1 * delta)
+
+func start():
+	start_flag = true
