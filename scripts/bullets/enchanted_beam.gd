da1d90caecf27dd8514c8c8cec7c424d2e4e5946
dpDeadlyPuzzles <1976985776@qq.com>
2024-06-06 20:46:53 +0800

    formatted

--- scripts/bullets/enchanted_beam.gd
+++ scripts/bullets/enchanted_beam.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
-@export var direction : Vector2
-@export var speed : float
+@export var direction: Vector2
+@export var speed: float
+
+
 # Called when the node enters the scene tree for the first time.
-func _ready(): 
+func _ready():
 	rotation = PI / 4 + Vector2(0, 0).angle_to_point(direction)
 	direction = direction.normalized()
 	set_process(0)
@@ -14,10 +16,11 @@ func _ready():
 func start():
 	set_process(1)
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += delta * speed * direction 
-		
+	position += delta * speed * direction
+
 
 func _on_out_screen_screen_exited():
 	queue_free()
--- scripts/weapons/star_wrath.gd
+++ scripts/weapons/star_wrath.gd
@@ -1,13 +1,9 @@
 extends Sprite2D
 
 var start_flag = true
-enum States{
-	Wave1,
-	Wave2,
-	WaveReSet,
-	None
-}
+enum States { Wave1, Wave2, WaveReSet, None }
 var state = States.Wave1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,12 +12,15 @@ func _ready():
 	if get_tree().current_scene == self:
 		$Fight_Timer.start()
 
+
 func wave():
 	self.state = States.Wave1
 	new_bullet()
 
-@export var star_wrath_bullet:PackedScene
-@export var BULLETS_NUM:int
+
+@export var star_wrath_bullet: PackedScene
+@export var BULLETS_NUM: int
+
 
 func new_bullet():
 	for i in range(BULLETS_NUM):
@@ -29,6 +28,7 @@ func new_bullet():
 		var sz = get_viewport_rect().size.x
 		star.init(sz, 0)
 		$"..".add_child.call_deferred(star)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -43,8 +43,10 @@ func _process(delta):
 	elif self.state == States.WaveReSet:
 		pass
 
+
 func start():
 	start_flag = true
+
 
 func _on_fight_t_imer_timeout():
 	new_bullet()
