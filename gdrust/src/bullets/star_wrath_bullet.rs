569b2a83cec8a7500a56716fa6ae04a554864f72
limuy <limuyang202011@163.com>
2024-06-07 21:35:32 +0800

    replace gdscript for star wrath with rust

--- gdrust/src/bullets/star_wrath_bullet.rs
+++ gdrust/src/bullets/star_wrath_bullet.rs
@@ -1,15 +1,24 @@
 use godot::engine::{Area2D, IArea2D};
 use godot::obj::WithBaseField;
 use godot::prelude::*;
+use rand::{thread_rng, Rng};
+use std::f32::consts::PI;
+
+use crate::utils::split_to_vec;
 
 #[derive(GodotClass)]
 #[class(base=Area2D)]
-struct StarWrathBullet {
+pub struct StarWrathBullet {
     base: Base<Area2D>,
     direct: Vector2,
+    speed: i32,
 }
 
 const INIT_DIRECT: Vector2 = Vector2::new(0.0, 0.0);
+const SPEED_MIN: i32 = 300;
+const SPEED_MAX: i32 = 400;
+const DIRECT_MIN: f32 = PI + PI / 3.0;
+const DIRECT_MAX: f32 = PI + PI / 3.0 * 2.0;
 
 #[godot_api()]
 impl IArea2D for StarWrathBullet {
@@ -17,6 +26,7 @@ impl IArea2D for StarWrathBullet {
         Self {
             base,
             direct: INIT_DIRECT,
+            speed: 0,
         }
     }
 
@@ -28,5 +38,28 @@ impl IArea2D for StarWrathBullet {
 
 #[godot_api()]
 impl StarWrathBullet {
-    fn init() {}
+    /// 带方向的初始化
+    pub fn init_with_direct(&mut self, direct: Vector2) {
+        self.direct = direct;
+        self.base_init((direct.y / direct.x).atan());
+    }
+
+    fn random_speed(&mut self) {
+        self.speed = rand::thread_rng().gen_range(SPEED_MIN..=SPEED_MAX);
+    }
+
+    fn base_init(&mut self, rad: f32) {
+        self.random_speed();
+        self.base_mut().show();
+        // 旋转到正确的位置
+        self.base_mut().rotate(rad);
+    }
+
+    /// 从左上角初始化弹幕，方向随机
+    pub fn init_from_corner(&mut self) {
+        // 随机方向
+        let direct_rad = thread_rng().gen_range(DIRECT_MIN..=DIRECT_MAX);
+        self.direct = split_to_vec(direct_rad);
+        self.base_init(direct_rad);
+    }
 }
--- gdrust/src/utils.rs
+++ gdrust/src/utils.rs
@@ -1,3 +1,5 @@
+use godot::prelude::*;
+
 #[macro_export()]
 macro_rules! godot_assert {
     ($($tok:tt)*) => {
@@ -5,4 +7,9 @@ macro_rules! godot_assert {
             godot::prelude::godot_error!("{} failed", stringify!($($tok)*))
         }
     };
+}
+
+/// 将传入的弧度转换成单位向量
+pub fn split_to_vec(vec: f32) -> Vector2 {
+    Vector2::new(vec.cos(), vec.sin())
 }
