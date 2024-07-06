af7a24b74f30b0ed6420fc3acef228ed6fb1861b
limuy <limuyang202011@163.com>
2024-07-06 14:27:53 +0800

    build:update deps

--- scripts/weapons/seedler.gd
+++ scripts/weapons/seedler.gd
@@ -3,11 +3,14 @@ extends Sprite2D
 @onready var path = $"../Path2D"
 @onready var follow = $"../Path2D/PathFollow2D"
 
-func move(length : float) -> void:
+
+func move(length: float) -> void:
 	follow.progress = length
 	position = follow.global_position
-	
-const times = 5	
+
+
+const times = 5
+
 
 func _ready() -> void:
 	rotation = 0
@@ -15,10 +18,11 @@ func _ready() -> void:
 	var start = 0
 	var end = path.curve.get_baked_length()
 	var mid = end / 2
-	var movement = create_tween().set_loops(times).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+	var movement = create_tween().set_loops(times).set_ease(Tween.EASE_IN).set_trans(
+		Tween.TRANS_SINE
+	)
 	movement.tween_method(move, start, mid, 1)
 	movement.tween_method(move, mid, end, 1)
 	var rotate = create_tween().set_loops(times)
 	rotate.tween_property(self, "rotation", TAU * 5, 2)
-	rotate.tween_callback(func():rotation = 0)
-	
+	rotate.tween_callback(func(): rotation = 0)
