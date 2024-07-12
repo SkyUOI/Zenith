f4b4704f325c684a6f0f2830e588a5ddb0aa111f
limuy <limuyang202011@163.com>
2024-07-12 23:35:35 +0800

    fix:star wrath has leave animation

--- scenes/weapons/star_wrath/star_wrath.gd
+++ scenes/weapons/star_wrath/star_wrath.gd
@@ -1,7 +1,10 @@
 extends StarWrath
 
 @export var star_wrath_origin: PackedScene
-var operations = [func(): self.fall_star_process()]
+@onready var animation_player = $AnimationPlayer
+@onready var star_wrath = $StarWrath
+
+var operations = [func(): self.fall_star_process(), func(): self.leave()]
 var operation_idx = 0
 
 
@@ -9,14 +12,22 @@ func next_operation():
 	if operation_idx >= operations.size():
 		$"..".attack_finished.emit()
 		return
-	operations[operation_idx].call()
+	var tmp = operation_idx
 	operation_idx += 1
+	operations[tmp].call()
 
 
 func fall_star_process():
 	for i in range(10):
 		self.fall_star()
 		await get_tree().create_timer(randf_range(1.0, 2.0)).timeout
+	next_operation()
+
+
+func leave():
+	await self.get_tree().create_timer(0.3).timeout
+	var tween = create_tween()
+	tween.tween_property(star_wrath, "global_position", Vector2(-100.0, -100), 2)
 	next_operation()
 
 
