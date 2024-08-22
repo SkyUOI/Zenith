e791d5e48a5ae880db7bfacf2fec6c389e740b22
dpDeadlyPuzzles <1976985776@qq.com>
2024-08-22 20:40:52 +0800

    feat: build scenes of terra_blade and the beam, add straight_move function

---
+++ scenes/bullets/terra_blade/terra_blade_beam.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var tween = create_tween()
+
+
+func straight_move(
+	from: Vector2, to: Vector2, time: float, ease: Tween.EaseType, trans: Tween.TransitionType
+):
+	rotation = (to - from).angle()
+	position = from
+	tween.tween_property(self, "position", to, time).set_ease(ease).set_trans(trans)
+
+
+func _on_out_screen_screen_exited():
+	queue_free()
+
+
+func _ready():
+	straight_move(Vector2(100, 100), Vector2(500, 500), 2, Tween.EASE_IN, Tween.TRANS_CUBIC)
