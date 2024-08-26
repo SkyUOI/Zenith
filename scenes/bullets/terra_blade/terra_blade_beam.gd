d2db25cf374eec8d3826b43198a6d5963b148a34
dpDeadlyPuzzles <1976985776@qq.com>
2024-08-26 17:26:15 +0800

    feat: add animation for terra_blade

--- scenes/bullets/terra_blade/terra_blade_beam.gd
+++ scenes/bullets/terra_blade/terra_blade_beam.gd
@@ -10,6 +10,19 @@ func straight_move(
 	position = from
 	tween.tween_property(self, "position", to, time).set_ease(ease_).set_trans(trans)
 
+func falling_move(
+	from: Vector2, velocity: Vector2, g: float
+):
+	var vx = velocity.x
+	var vy = velocity.y
+	rotation = Vector2(vx, vy).angle()
+	var f = func(t):
+		var x = vx * t
+		var y = vy * t + g * t * t / 2.0
+		position = from + Vector2(x, y)
+		rotation -= Vector2.DOWN.angle_to(Vector2(cos(rotation), sin(rotation))) / 70
+	tween.tween_method(f, 0.0, 10.0, 10)
+	
 
 func _on_out_screen_screen_exited():
 	queue_free()
--- scenes/weapons/terra_blade/terra_blade.gd
+++ scenes/weapons/terra_blade/terra_blade.gd
@@ -6,9 +6,20 @@ var tween = create_tween()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	begin()
+	
+	tween.tween_callback(func():
+		await get_tree().create_timer(0.2).timeout
+		line_call.call(Vector2(200, 200), Vector2(700,450), 10, shot_falling_beam)
+	)
+	tween.tween_property($Sprite2D, "position", Vector2(700, 450), 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_interval(10)
+
+func line_call(from, to, num, f):
+	for i in range(0, num + 1):
+		f.call(from + (to - from) * i / num)
 
 func begin():
-	$Sprite2D.position = Vector2(-500, 1000)
+	$Sprite2D.position = Vector2(200, 1000)
 	var center = Vector2(570, 200)
 	
 	# 绘制特效
@@ -25,9 +36,23 @@ func begin():
 			get_parent().add_child.call_deferred(beam)
 			var rad = r + unit_rad * i
 			var to = 2000 * Vector2(cos(rad), sin(rad))
-			beam.straight_move(center, center + to, 1.5, Tween.EASE_IN, Tween.TRANS_CIRC)
+			beam.straight_move(center, center + to, 1.2, Tween.EASE_IN, Tween.TRANS_CIRC)
 		
-	tween.tween_callback(radial_shot.bind(24, 0))
-	tween.tween_interval(0.2)
-	tween.tween_callback(radial_shot.bind(24, TAU / 48))
+	tween.tween_callback(func():
+		var t = create_tween()
+		t.tween_callback(radial_shot.bind(24, 0))
+		t.tween_interval(0.2)
+		t.tween_callback(radial_shot.bind(24, TAU / 48))
+		)
+	tween.tween_property($Sprite2D, "position", Vector2(200, 200), 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.parallel().tween_property($Sprite2D, "rotation", TAU + (Vector2(700, 450) - Vector2(200, 200)).angle(), 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC).as_relative()
 	
+var seed = PI
+func shot_falling_beam(center):
+	var beam = terra_blade_beam.instantiate()
+	get_parent().add_child.call_deferred(beam)
+	var to = 300 * Vector2(cos(seed), sin(seed))
+	beam.falling_move(center, to, 800)
+	seed = sqrt(seed * seed * seed * 114) / 2
+	while seed > 114:
+		seed /= TAU
